9003847d45ebf1ba12c261333be5de7ce0cf002e
wadim.mir0602 <wadim.mir0602@gmail.com>
2023-01-12 17:31:57 +0100

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './app.css';
+
+
+const App = () => {
+    return (
+        <div className="main_div">
+             
+
+
+        </div>
+    )  
+}
+
+
+
+export default App;
